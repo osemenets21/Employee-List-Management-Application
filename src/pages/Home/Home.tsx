d87aef15bf0b9715576b72a7f8e496a5ede8a121
osemenets21 <oleg.min.vin@gmail.com>
2023-10-31 21:11:55 +0100

    add base font-family for document, add style for home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Home.scss'; 
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.scss";
+import employeeFirst from './images/employee-first.png';
+import employeeSecond from './images/employee-second.png';
+
 
 export const Home = () => {
   return (
-    <div className="container"> 
-      <h1 className="heading">Welcome!</h1>
-      <Link to="/workers-list" className="button">
-        Our employee
-      </Link>
+    <div className="container">
+      <div className="background-line"></div>
+      <img src={employeeFirst} alt="workers-first-img" className="workers-first-img"/>
+      <div className="heading">
+        <h1 className="heading-text">
+          We take joy in every success achieved side by side with our amazing
+          clients
+        </h1>
+        <Link to="/workers-list" className="heading-button">
+          Our employee
+        </Link>
+      </div>
+      <img src={employeeSecond} alt="workers-second-img" className="workers-second-img"/>
     </div>
   );
 };
