5f92c86c0cc57c2475f09618cb1e49cfa4d0f117
osemenets21 <oleg.min.vin@gmail.com>
2023-10-31 19:14:39 +0100

    add folder mixins and create them

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import './Home.scss'
+import { Link } from 'react-router-dom';
+import './Home.scss'; 
 
 export const Home = () => {
   return (
-    <div className="gradient__text">Home</div>
-  )
-}
+    <div className="container"> 
+      <h1 className="heading">Welcome!</h1>
+      <Link to="/workers-list" className="button">
+        Our employee
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
