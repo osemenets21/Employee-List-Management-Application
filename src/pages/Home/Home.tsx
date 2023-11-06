9ce3c250be2def9a2f4840411d7d6592765daeec
osemenets21 <oleg.min.vin@gmail.com>
2023-11-06 18:24:24 +0100

    change home for animation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,25 +1,61 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Home.scss";
-import employeeFirst from './images/employee-first.png';
-import employeeSecond from './images/employee-second.png';
 
 
 export const Home = () => {
   return (
-    <div className="container">
-      <div className="background-line"></div>
-      <img src={employeeFirst} alt="workers-first-img" className="workers-first-img"/>
-      <div className="heading">
-        <h1 className="heading-text">
-          We take joy in every success achieved side by side with our amazing
-          clients
-        </h1>
-        <Link to="/workers-list" className="heading-button">
-          Our employee
-        </Link>
-      </div>
-      <img src={employeeSecond} alt="workers-second-img" className="workers-second-img"/>
+    <div className="wrapper">
+        <div className="main"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
+        <div className="overlay"></div>
     </div>
   );
 };
