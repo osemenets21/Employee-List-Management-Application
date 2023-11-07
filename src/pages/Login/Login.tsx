5d235402f4304ea139ca5737676ff7bcd2b690f4
osemenets21 <oleg.min.vin@gmail.com>
2023-11-07 19:28:01 +0100

    Create page Login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Login.scss';
 
-export const Login = () => {
+export const Login: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Add your logic for checking the username and password here.
+    // For example, you can use state or make a request to a server.
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login-page">
+      <div className="login-form">
+        <h1>Login</h1>
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button onClick={handleLogin}>Login</button>
+      </div>
+    </div>
+  );
+};
