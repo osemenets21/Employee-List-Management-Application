d57e0dcf0de8bc562913cf9b1018e12c964f78f7
Radek11111 <131595727+Radek11111@users.noreply.github.com.>
2023-11-20 09:18:33 +0100

    add styles to page Login and Sign up

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import './SignUp.scss';
+import { Link, useNavigate } from "react-router-dom";
 
 export const SignUp: React.FC = () => {
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [passwordError, setPasswordError] = useState('');
-  const [email, setEmail] = useState('');
-  const [emailError, setEmailError] = useState('');
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [userName, setUserName] = useState("");
 
   const validateEmail = (email: string) => {
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -18,8 +20,8 @@ export const SignUp: React.FC = () => {
   };
 
   const handleSignUp = () => {
-    setEmailError('');
-    setPasswordError('');
+    setEmailError("");
+    setPasswordError("");
 
     if (!validateEmail(email)) {
       setEmailError("Invalid email address");
@@ -40,42 +42,54 @@ export const SignUp: React.FC = () => {
     // For example, you can make a request to a server to create the account.
 
     // Reset the password and email error messages
-    setPasswordError('');
-    setEmailError('');
+    setPasswordError("");
+    setEmailError("");
   };
 
   return (
-    <div className="signup-page dark:bg-slate-500">
-      <div className="signup-form dark:bg-slate-800 dark:border-slate-800">
-        <h1 className='dark:text-white'>Sign Up</h1>
-        <div className={`input-group ${emailError ? 'error' : ''}`}>
-          <input
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className={`input-group ${passwordError ? 'error' : ''}`}>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <div className={`input-group ${passwordError ? "error" : ""}`}>
           <input
             type="password"
-            placeholder="Password"
+            placeholder="password"
+            className="border p-3 py-3 rounded-lg w-full"
+            id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <div className={`input-group ${passwordError ? 'error' : ''}`}>
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </div>
         {emailError && <div className="error-message">{emailError}</div>}
         {passwordError && <div className="error-message">{passwordError}</div>}
-        <button className='dark:bg-slate-500' onClick={handleSignUp}>Sign Up</button>
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          {"Sign Up"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to={"/Login"}>
+          <span className="text-blue-700">Sign in</span>
+        </Link>
       </div>
+      {<p className="text-red-500 mt-5"></p>}
     </div>
   );
 };
