ad911b213a5d7d093026c48f4faed0d93b070d69
osemenets21 <oleg.min.vin@gmail.com>
2023-11-25 12:55:40 +0100

    check server to cyclic

--- src/context/WorkersListContext.tsx
+++ src/context/WorkersListContext.tsx
@@ -22,7 +22,7 @@ export const WorkersListContextProvider: React.FC<{ children: ReactNode }> = ({
   const getWorkers = useCallback(async (page: number, limit: number) => {
     try {
       const response = await fetch(
-        `http://localhost:5000/workerList?_page=${page}&_limit=${limit}`
+        `https://fair-teal-puppy-veil.cyclic.app/api/workerList?_page=${page}&_limit=${limit}`
       );
       if (!response.ok) {
         throw new Error("Problem z serwerem");
@@ -44,7 +44,7 @@ export const WorkersListContextProvider: React.FC<{ children: ReactNode }> = ({
 
   const addWorker = async (newWorkerData: Workers) => {
     try {
-      const response = await fetch("http://localhost:5000/workerList", {
+      const response = await fetch("https://fair-teal-puppy-veil.cyclic.app/api/workerList", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -66,7 +66,7 @@ export const WorkersListContextProvider: React.FC<{ children: ReactNode }> = ({
   const editWorker = async (updatedWorkerData: Workers) => {
     try {
       const response = await fetch(
-        `http://localhost:5000/workerList/${updatedWorkerData.id}`,
+        `https://fair-teal-puppy-veil.cyclic.app/api/workerList/${updatedWorkerData.id}`,
         {
           method: "PUT",
           headers: {
@@ -93,7 +93,7 @@ export const WorkersListContextProvider: React.FC<{ children: ReactNode }> = ({
   const deleteWorker = async (workerId: number) => {
     try {
       const response = await fetch(
-        `http://localhost:5000/workerList/${workerId}`,
+        `https://fair-teal-puppy-veil.cyclic.app/api/workerList/${workerId}`,
         {
           method: "DELETE",
         }
