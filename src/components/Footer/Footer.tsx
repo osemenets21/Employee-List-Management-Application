c1a0a45ce8a9732ebf27f48edc088b530b8634a0
osemenets21 <oleg.min.vin@gmail.com>
2023-12-05 17:54:12 +0100

    correct classes to BEM for footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,11 +5,11 @@ import { AiFillFacebook, AiFillGithub, AiFillLinkedin, AiOutlineInstagram } from
 export const Footer = () => {
   return (
     <footer className="footer dark:bg-slate-800">
-      <div className="social-media">
-        <a href="https://www.facebook.com/brlltman/"><AiFillFacebook className="footer-icons"/></a>
-        <a href="https://www.linkedin.com/in/oleh-semenets/"><AiFillLinkedin className="footer-icons"/></a> 
-        <a href="https://www.instagram.com/brlltman/"><AiOutlineInstagram className="footer-icons"/></a> 
-        <a href="https://github.com/osemenets21"><AiFillGithub className="footer-icons github-ico"/></a> 
+      <div className="footer-media">
+        <a href="https://www.facebook.com/brlltman/"><AiFillFacebook className="footer-media-icons"/></a>
+        <a href="https://www.linkedin.com/in/oleh-semenets/"><AiFillLinkedin className="footer-media-icons"/></a> 
+        <a href="https://www.instagram.com/brlltman/"><AiOutlineInstagram className="footer-media-icons"/></a> 
+        <a href="https://github.com/osemenets21"><AiFillGithub className="footer-media-icons github-ico"/></a> 
       </div>
       <div className="footer-text">© Warsaw, Poland</div>
     </footer>
